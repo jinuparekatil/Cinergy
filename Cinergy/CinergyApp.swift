0159766ffe3dc24a95d52f018e6504b72271a492
Jinu Joy <jinuparekatil@gmail.com>
2024-01-19 18:30:40 +0530

    Initial Commit

---
+++ Cinergy/CinergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinergyApp.swift
+//  Cinergy
+//
+//  Created by Jinu on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CinergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
